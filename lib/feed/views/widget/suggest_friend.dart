b7908377aa9906d65bd68b4aef9147d43d8670ae
Apisit <amornchanchaigul@gmaul.com>
2026-01-18 12:28:55 +0700

    edit suggest feed

--- lib/feed/views/widget/suggest_friend.dart
+++ lib/feed/views/widget/suggest_friend.dart
@@ -10,6 +10,28 @@ class SuggestFriend extends StatefulWidget {
 class _SuggestFriendState extends State<SuggestFriend> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          for (var i in [1, 2, 3, 4, 5])
+            Card(
+              margin: EdgeInsets.all(5),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      "assets/images/profiles/samurai_man.png",
+                      width: 75,
+                    ),
+                    Text("Samurai Man"),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
